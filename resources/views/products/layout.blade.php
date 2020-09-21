9c7312513da8fce9a2f71e63d8e5fdfb4e7242ff
Katheesh <katheeskumar@outlook.com>
2020-09-21 13:35:13 +0530

    added csrf token

--- resources/views/products/layout.blade.php
+++ resources/views/products/layout.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>Laravel 7 CRUD Application - ItSolutionStuff.com</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+    <meta name="_token" content="{!! csrf_token() !!}" />
 </head>
 <body>
   
